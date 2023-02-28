e7464e39fa238772a6020e7a4eea20b91eaf949d
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-02-28 21:04:36 +0100

    changed to absolute filepaths

--- elkStack/scripts/policies/create_policies.sh
+++ elkStack/scripts/policies/create_policies.sh
@@ -7,19 +7,19 @@ curl -u elastic:superuser --cacert /home/ubuntu/elasticsearch-ca.pem \
 -X POST "https://localhost:5601/api/fleet/agent_policies?sys_monitoring=true" \
 -H "Content-Type: application/json" \
 -H "kbn-xsrf: true" \
--d @elkStack_DCSG2003/elkStack/policy/apache/create_bookface_apache.json
+-d @/home/ubuntu/elkStack_DCSG2003/elkStack/policy/apache/create_bookface_apache.json
 
 # Haproxy
 curl -u elastic:superuser --cacert /home/ubuntu/elasticsearch-ca.pem \
 -X POST "https://localhost:5601/api/fleet/agent_policies?sys_monitoring=true" \
 -H "Content-Type: application/json" \
 -H "kbn-xsrf: true" \
--d @elkStack_DCSG2003/elkStack/policy/haproxy/create_bookface_haproxy.json
+-d @/home/ubuntu/elkStack_DCSG2003/elkStack/policy/haproxy/create_bookface_haproxy.json
 
 # cockroachDB
 curl -u elastic:superuser --cacert /home/ubuntu/elasticsearch-ca.pem \
 -X POST "https://localhost:5601/api/fleet/agent_policies?sys_monitoring=true" \
 -H "Content-Type: application/json" \
 -H "kbn-xsrf: true" \
--d @elkStack_DCSG2003/elkStack/policy/cockroachDB/create_bookface_cockroachDB.json
+-d @/home/ubuntu/elkStack_DCSG2003/elkStack/policy/cockroachDB/create_bookface_cockroachDB.json
 
