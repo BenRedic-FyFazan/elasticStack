e520e5ff81d987749d67de953712047a82ea9d69
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-02-28 11:42:57 +0100

    changed ip addresses in integration scripts to refer to localhost

--- elkStack/scripts/integrations/integration_haproxy.sh
+++ elkStack/scripts/integrations/integration_haproxy.sh
@@ -3,7 +3,7 @@
 echo "Starting integration_haproxy.sh"
 
 curl -u elastic:superuser --cacert elasticsearch-ca.pem \
--X POST "https://192.168.130.186:5601/api/fleet/epm/packages/haproxy/1.5.0" \
+-X POST "https://localhost:5601/api/fleet/epm/packages/haproxy/1.5.0" \
 -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d \
 '{
   "force": true
