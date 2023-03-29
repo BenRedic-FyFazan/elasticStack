fb9c664600f207d7dd3d9d0a4480a2cdba36b35c
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-03-29 13:49:38 +0200

    Completed the bf_uc_reports.sh changes, it now splits the reports from within the JSON received via 'uc reports' into individual log-entries and stores them in logfiles according to type

---
+++ client/envs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Elk components
+export elasticSearch="" 
+export kibanaURL=""
+
+## Locations
+export elasticStack="$HOME/elasticStack"
+export client="$HOME/elasticStack/client"
+export manager="$HOME/elasticStack/client/manager"
--- client/manager/script/bf_uc_reports.sh
+++ client/manager/script/bf_uc_reports.sh
@@ -7,16 +7,19 @@ source "$openstack_auth"
 
 json=$(uc reports)
 
-# Groups the nested objects in the received JSON by their "type" value then
-# iterates through the objects, extracts individual reports and appends it to a file corresponding to its type
+# Groups the nested objects in the received JSON by their "type" value then iterates through
 grouped_json=$(echo "$json" | jq 'reduce .Reports[] as $item ({}; .[$item.type] += [$item])')
 for type in $(echo "$grouped_json" | jq -r 'keys[]'); do
 
     LOG_FILE="/var/log/bookface/bf_uc_reports_${type}.log"
     objects=$(echo "$grouped_json" | jq --arg type "$type" '.[$type]')
 
-    # Iterates through each object, renames "check_time" to "@timestamp", and then appends them.
+    # Iterates through each object(within the "type" grouped objects)
+    # renames "check_time" to "@timestamp", and appends it as a single JSON object
+    # Converts the string stored in the "vmcount" field into an easier to parse nested object
     echo "$objects" | jq -rc '.[] | with_entries(if .key == "check_time" then .key = "@timestamp" else . end) | @json' | while IFS= read -r object; do
+        object=$(echo "$object" | jq -rc 'del(._id, .name, .type) | @json')
+        object=$(echo "$object" | jq -rc 'if .vmcount | type == "string" then .vmcount |= fromjson | @json else . | @json end')
         echo "$object" >> "$LOG_FILE"
     done
 
