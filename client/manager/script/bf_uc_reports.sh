3239100db3c2c3ad7fb1d859fb0c48ff5165d6a2
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-03-30 13:15:44 +0200

    Component templates and index templates for uc reports

--- client/manager/script/bf_uc_reports.sh
+++ client/manager/script/bf_uc_reports.sh
@@ -16,7 +16,6 @@ for type in $(echo "$grouped_json" | jq -r 'keys[]'); do
 
     # Iterates through each object(within the "type" grouped objects)
     # renames "check_time" to "@timestamp", and appends it as a single JSON object
-    # Converts the string stored in the "vmcount" field into an easier to parse nested object
     echo "$objects" | jq -rc '.[] | with_entries(if .key == "check_time" then .key = "@timestamp" else . end) | @json' | while IFS= read -r object; do
         object=$(echo "$object" | jq -rc 'del(._id, .name, .type) | @json')
         object=$(echo "$object" | jq -rc 'if .vmcount | type == "string" then .vmcount |= fromjson | @json else . | @json end')
