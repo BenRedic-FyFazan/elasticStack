89c54ba2741aa481c5d85307878d29da048d4cda
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-03-31 16:11:57 +0200

    .

---
+++ client/manager/uninstall_manager.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+# Variables
+manager="$HOME/elasticStack/client/manager"
+vars="$HOME/elasticStack/vars"
+elastic_ca="$HOME/elasticStack/vars/elasticsearch-ca.pem"
+elastic_ip=$(cat $HOME/elasticStack/vars/elasticsearch_ip.txt)
+filebeat_yml="$HOME/elasticStack/vars/filebeat.yml"
+search_dir="/"
+openstack_auth_location="$HOME/elasticStack/client/manager/openstack_auth.txt"
+export logrotated="/etc/logrotate.d/"
+export log_loc="/var/log/bookface/"
+export log_uc_reports="/var/log/bookface/bf_uc_reports.log"
+export log_os_servers="/var/log/bookface/bf_os_servers.log"
+bf_os_servers_script="$HOME/elasticStack/client/manager/script/bf_os_servers.sh"
+bf_uc_reports_script="$HOME/elasticStack/client/manager/script/bf_uc_reports.sh"
+
+
+echo "Uninstalling and reverting changes..."
+
+# Remove CRON jobs
+cronjob_1="* * * * * $manager/script/bf_os_servers.sh"
+cronjob_2="*/5 * * * * $manager/script/bf_uc_reports.sh"
+temp_file=$(mktemp)
+crontab -l | grep -v -F "$cronjob_1" | grep -v -F "$cronjob_2" > "$temp_file"
+crontab "$temp_file"
+rm "$temp_file"
+
+echo "Uninstallation and reversion complete."
+
